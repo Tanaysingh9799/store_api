0839b24cc24270fdbcbb09dffa56d24cf028eff0
Tanaysingh9799 <64830765+Tanaysingh9799@users.noreply.github.com>
2022-05-28 02:11:11 +0530

    Add files via upload
    app.py - working and building of api
    books.py - books data

---
+++ books.py
@@ -0,0 +1,26 @@
+books = [
+    {
+        "id": 1,
+        "price": "Rs500",
+        "book_title": "A Gentleman in Moscow",
+        "book_author": "Amor Towles",
+        "publisher": "Amor Towles",
+        "description": 'Brimming with humor'
+    },
+    {
+        "id": 2,
+        "price": "Rs650",
+        "book_title": "Brimming with humor",
+        "book_author": "Maggie O'Farrell",
+        "publisher": "Maggie O'Farrell",
+        "description": 'extraordinary, eccentric'
+    },
+    {
+        "id": 3,
+        "price": "Rs600",
+        "book_title": "The Vanishing Half",
+        "book_author": "Brit Bennett",
+        "publisher": "Brit Bennett",
+        "description": 'American history of passing'
+    },
+]
